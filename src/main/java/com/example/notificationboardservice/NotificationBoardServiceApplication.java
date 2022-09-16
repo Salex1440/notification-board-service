46ba65921f1b4ae616f7b2a10e63f07ef0997793
Salex <123@13.com>
2022-09-16 09:29:22 +0300

    Initial commit

---
+++ src/main/java/com/example/notificationboardservice/NotificationBoardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.notificationboardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationBoardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationBoardServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/notificationboardservice/NotificationBoardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notificationboardservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationBoardServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
